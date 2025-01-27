6ef572aef490baadf47a3500af1c8f82da5e1544
Krish Bhuva <bhuvakrish03@gmail.com>
2025-01-27 15:03:50 +0530

    password generator project

--- 04BgChanger/src/App.jsx
+++ 04BgChanger/src/App.jsx
@@ -4,7 +4,7 @@ import { useState } from "react"
 function App() {
   let [color, setColor] = useState("gold")
   return (
-    
+
       <div className="w-full h-screen duration-200" style={{backgroundColor:color}}>
         <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
           <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
@@ -12,7 +12,7 @@ function App() {
             onClick={()=>setColor("red")}
             className="outline-none px-4 py-1 rounded-full text-black shadow-lg" 
             style={{backgroundColor:"red"}}>red</button>
-            <button 
+            <button
             onClick={()=>setColor("white")}
             className="outline-none px-4 py-1 rounded-full text-black shadow-lg" 
             style={{backgroundColor:"white"}}>white</button>
