2b35950a38449aa9e8e5527caec328fe8885adb9
Krish Bhuva <bhuvakrish03@gmail.com>
2025-02-11 19:27:55 +0530

    revision of hooks

--- 02Counter/src/App.jsx
+++ 02Counter/src/App.jsx
@@ -2,29 +2,36 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [counter, setCounter] = useState(15)
 
-  let [counter, setcounter] = useState(0)
-    if(counter < 0){
-      setcounter(0)
-    }else if(counter > 20){
-      setcounter(20)
-    }
   function addValue(){
-    setcounter(counter++)
-  } 
+    setCounter(counter + 1)
+    if(counter >= 5){
+      setCounter(5)
+    }
+  }
+
   function removeValue(){
-    setcounter(counter--)
-  } 
+    setCounter(counter - 1)
+    if(counter <= 0){
+      setCounter(0)
+    }
+  }
+
   return (
     <>
-      <h1>Coffee or Code</h1>
-      <h2>Cunter Value {counter}</h2>
+      <h1>Chai or React</h1>
+      <h2>counter valur:{counter}</h2>
 
-      <button onClick={addValue}>Add Value</button>
+
+      <button 
+      onClick={addValue}
+      >add value</button>
       <br />
-      <button onClick={removeValue}>Remove Value</button>
+      <button
+      onClick={removeValue}
+      >remove value</button>
     </>
   )
 }
-
 export default App
